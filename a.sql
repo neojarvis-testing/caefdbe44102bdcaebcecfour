1b74736627b896a3e944e824cbfc3b5d051f2080
neojarvis <techadmin@iamneo.ai>
2024-02-06 09:37:48 +0000

    Auto-save-commit-2/6/2024--3:07:42-PM

---
+++ a.sql
@@ -0,0 +1,11 @@
+    -- Create the table
+    CREATE TABLE Example3 (
+        ID int,
+        Name nvarchar(255)
+    );
+
+    -- Insert values into the table
+    INSERT INTO Example3 (ID, Name) VALUES (1, 'qwerty');
+    INSERT INTO Example3 (ID, Name) VALUES (2, 'pop');
+
+-- Select and view all data from the tabl
