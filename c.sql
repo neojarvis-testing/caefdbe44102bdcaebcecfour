93652b1825e204aa1394afab20d6e0a5d83ac4a1
neojarvis <techadmin@iamneo.ai>
2024-02-06 10:13:46 +0000

    Manual-save-commit-2/6/2024--3:43:37-PM

---
+++ c.sql
@@ -0,0 +1,12 @@
+    -- Create the table
+    CREATE TABLE Example4 (
+        ID int,
+        Name nvarchar(255)
+    );
+
+    -- Insert values into the table
+    INSERT INTO Example4 (ID, Name) VALUES (1, 'one');
+    INSERT INTO Example4 (ID, Name) VALUES (2, 'two');
+    INSERT INTO Example4 (ID, Name) VALUES (3, 'three');
+
+-- Select and view all data from the tabl
